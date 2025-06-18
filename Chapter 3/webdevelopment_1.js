fb79070ea06e1dbe6c22553350ea0223e53cafe0
Shafayath Ulla Khan <shafayathullakhan1998@gmail.com>
2025-06-18 20:19:34 +0530

    latest commit

---
+++ Chapter 3/webdevelopment_1.js
@@ -0,0 +1,18 @@
+// micro services
+
+// mvc
+
+// model
+// represents data
+// represents business logic
+
+// views
+// user interface
+// displays data
+
+// controller
+// handles requests
+// updates model
+// send responses
+
+// mvc project by using express
